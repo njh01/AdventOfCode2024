7574233bf98305c6cc63a63c0ab428a4ac2449f4
Nate Henty <nate.henty99@gmail.com>
2024-12-10 22:04:50 -0600

    removed \S

--- day3/problem3.py
+++ day3/problem3.py
@@ -5,7 +5,7 @@ import re
 def problem3_part1(file):
     fopen = open(file,'rb')
     data = str(fopen.read())
-    mul_list = re.findall("\Smul\(\d+,\d+\)",data)
+    mul_list = re.findall("mul\(\d+,\d+\)",data)
     sum = 0
     for entry in mul_list:
         numbers = re.findall("\d+",entry)
