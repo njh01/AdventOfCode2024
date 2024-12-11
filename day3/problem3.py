d3eba7763783f706182568155edf32b613b6d9f0
Nate Henty <nate.henty99@gmail.com>
2024-12-10 22:57:56 -0600

    Looked at 7, attempted 3pt2

--- day3/problem3.py
+++ day3/problem3.py
@@ -13,6 +13,29 @@ def problem3_part1(file):
         sum += product
     print(f"The sum of valid products is {sum}")
 
+def problem3_part2(file):
+    fopen = open(file,'rb')
+    data = str(fopen.read())
+    instructions = re.findall("mul\(\d+,\d+\)|do\(\)|don't\(\)",data)
+    if instructions[0] == "do()":
+        valid = True
+    else: 
+        valid = False
+    sum = 0
+    for entry in instructions:
+        if entry == 'do()':
+            valid = True
+        elif entry == "don't()":
+            valid = False
+        else:
+            if valid:
+                numbers = re.findall("\d+",entry)
+                product = int(numbers[0]) * int(numbers[1])
+                sum += product
+    print(f"The sum of valid products in part 2 is {sum}")
+
 if __name__ == '__main__':
     filein = 'input.txt'
+    second_file = 'input2.txt'
     problem3_part1(filein)
+    problem3_part2(second_file)
---
+++ day7/problem7.py
@@ -0,0 +1,8 @@
+# Advent of Code 2024
+# day7
+def problem7_part1(file):
+    return -999
+
+if __name__ == '__main__':
+    filein = 'input.txt'
+    problem7_part1(filein)
