c02683805f552116a179faec1176134c2f98ead4
Nate Henty <nate.henty99@gmail.com>
2024-12-15 18:49:03 -0600

    Day 13 part 2

--- day13/problem13.py
+++ day13/problem13.py
@@ -29,6 +29,35 @@ def problem13_part1(file):
             cost += 3 * a_press + b_press
     print(f"Total cost: {cost}")
 
+def problem13_part2(file):
+    """
+    a_press * ax + b_press * bx = tx
+    a_press * ay + b_press * by = ty
+    """
+    with open(file,'r') as f:
+        data = f.readlines()
+    cost = 0
+    i = 0
+    while i < len(data):
+        ax, ay = re.findall("\d+",data[i])
+        i += 1
+        bx, by = re.findall("\d+",data[i])
+        i += 1
+        tx, ty = re.findall("\d+",data[i])
+        i += 2
+        tx = int(tx)
+        ty = int(ty)
+        tx += 10000000000000
+        ty += 10000000000000
+        mat = Matrix([[ax, bx, tx],[ay,by,ty]])
+        solution = mat.rref()
+        if float(solution[0][0,2])%1 == 0 and float(solution[0][1,2])%1 == 0:
+            a_press = solution[0][0,2]
+            b_press = solution[0][1,2]
+            cost += 3 * a_press + b_press
+    print(f"Total cost, part 2: {cost}")
+
 if __name__ == '__main__':
     file = "input.txt"
     problem13_part1(file)
+    problem13_part2(file)
